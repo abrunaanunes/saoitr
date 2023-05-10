2de9c5b9046f1362574dc55f6f9b60a876b08113
Bruna Nunes <bruna@websix.com.br>
2023-05-09 22:28:19 -0300

    Alterações CORS

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
-const { router } = require("./src/routes/routes.js")
-const { database } = require("./src/config/database")
-const express = require("express")
+const { router } = require('./src/routes/routes')
+const { database } = require('./src/config/database')
+const express = require('express')
 const expressValidator = require('express-validator')
 const app = express()
 const PORT = process.env.SERVER_PORT
@@ -8,7 +8,7 @@ const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN
 
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN)
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', '*')
     res.setHeader('Access-Control-Allow-Headers', '*')
     res.setHeader('Access-Control-Allow-Credentials', true)
@@ -17,8 +17,8 @@ app.use((req, res, next) => {
 app.use(router)
 
 
-database.on("error", console.log.bind(console, "Connection error"))
-database.once("open", () => { console.log("Successful connection") })
+database.on('error', console.log.bind(console, 'Connection error'))
+database.once('open', () => { console.log('Successful connection') })
 
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
