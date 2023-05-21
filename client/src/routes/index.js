d7e5caada88162b510005bae584323bac3ccd0de
Bruna Nunes <bruna@websix.com.br>
2023-05-21 20:28:15 -0300

    Redirecionamento após autenticação

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Home from "../pages/Home"
 import Login from "../pages/Login"
 import Occurrences from "../pages/Occurrences"
@@ -12,14 +12,15 @@ const Private = ({ Item }) => {
 }
 
 const RoutesApp = () => {
+    const { authenticated } = useAuth() 
     return (
         <BrowserRouter>
             <Fragment>
                 <Routes>
-                    {/* <Route exact path="/" element={<Home></Home>}></Route> */}
-                    <Route exact path="/" element={<Register></Register>}></Route>
+                    <Route exact path="/" element={<Home></Home>}></Route>
+                    <Route exact path="/register" element={ authenticated ?  <Navigate replace to="/" /> : <Register/> }></Route>
                     <Route path="/occurrences" element={<Private Item={Occurrences}/>}></Route>
-                    <Route path="/login" element={<Login></Login>}></Route>
+                    <Route path="/login" element={ authenticated ?  <Navigate replace to="/" /> : <Login/> }></Route>
                 </Routes>
             </Fragment>
         </BrowserRouter>
